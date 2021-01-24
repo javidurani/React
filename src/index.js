e0960384d8340d9b29fb8b3695b7b8ec993254bf
javidurani <jduraniglesias21122@columbushs.com>
2021-01-24 13:58:47 -0800

    more notes

--- src/index.js
+++ src/index.js
@@ -31,3 +31,39 @@ function Hi(){
 ReactDOM.render(<Hi/>, document.querySelector('#root'));
 
 //the code will compile in babel to a simpler version that is pure javascript
+
+//simple function that calls the name var and greets 
+function greet(name){
+    return "hi " + name;
+}
+
+//working with props
+//new parameter will be props
+function Hi(props){
+    return <div><strong>Hello {props.name}!</strong>
+    <h1>Welcome to React</h1>
+    <h2>So far all is good</h2>
+    <li>test 1</li>
+    <li>test 2</li>
+
+    {5 + 10}
+    </div>
+}
+
+//the props name must be passed and we do this by doing this
+//it will then say "Hello Javi!"
+//you can also pass in booleans, nums, etc. (not limited to strings)
+
+ReactDOM.render(<Hi name="Javi"/>), document.querySelector('#root')
+
+//these bars {} also determine whether somethings a string or js function
+
+<Sidebar hidden ="false"/> //string
+<Sidebar hidden = {false}/> //js expression
+//there can also be multiple props
+function HiFullName(props) {
+    return(<div>Hi {props.firstname} {props.lastname}!</div>)
+}
+
+//here we define what the firstname and lastname are
+<HiFullName firstname = "Javier" lastname = "Duran"/>
